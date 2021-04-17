d8064ca40c2c669502fe434677e422081f5bc5e2
Mert Koprucu <mert.koprucu@siemens.com>
2021-04-17 12:47:36 +0300

    Configure parameters are added.

--- IdentityServerLearning/AuthServer/Config/Config.cs
+++ IdentityServerLearning/AuthServer/Config/Config.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Security.Claims;
 using IdentityServer4.Models;
+using IdentityServer4.Test;
 
 namespace AuthServer.Config
 {
@@ -54,7 +56,40 @@ namespace AuthServer.Config
         }
         #endregion
 
+        public static IEnumerable<TestUser> GetTestUsers()
+        {
+            return new List<TestUser> {
+                new TestUser {
+                    SubjectId = "test-user1",
+                    Username = "test-user1",
+                    Password = "12345",
+                    Claims = {
+                        new Claim("name","test user1"),
+                        new Claim("website","https://wwww.testuser1.com"),
+                        new Claim("gender","1")
+                    }
+                },
+                new TestUser {
+                    SubjectId = "test-user2",
+                    Username = "test-user2",
+                    Password = "12345",
+                    Claims = {
+                        new Claim("name","test user2"),
+                        new Claim("website","https://wwww.testuser2.com"),
+                        new Claim("gender","0")
+                    }
+                }
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
     }
-
-
 }
