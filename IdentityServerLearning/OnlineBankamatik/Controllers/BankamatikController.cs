f15d989ada61eaaeb7b1c4eb7d11d016f143359e
Mert Koprucu <mert.koprucu@siemens.com>
2021-04-17 22:26:53 +0300

    OnlineBankamatik MVC App introduced.

---
+++ IdentityServerLearning/OnlineBankamatik/Controllers/BankamatikController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineBankamatik.Controllers
+{
+    public class BankamatikController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult OdemeYap()
+        {
+            return View();
+        }
+    }
+}
