577b7db825d7f4029a76f1ce8c21baaae014e490
Mert Koprucu <mert.koprucu@siemens.com>
2021-04-18 18:59:55 +0300

    Authorizing some endpoints.

--- IdentityServerLearning/OnlineBankamatik/Controllers/BankamatikController.cs
+++ IdentityServerLearning/OnlineBankamatik/Controllers/BankamatikController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace OnlineBankamatik.Controllers
@@ -15,8 +17,17 @@ namespace OnlineBankamatik.Controllers
         }
 
         [Authorize]
-        public IActionResult OdemeYap()
+        public async Task<IActionResult> OdemeYap()
         {
+            var authenticationProperties = (await HttpContext.AuthenticateAsync()).Properties.Items;
+            string accessToken = authenticationProperties.FirstOrDefault(x => x.Key == ".Token.access_token").Value;
+
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken}");
+            HttpResponseMessage responseMessage = await httpClient.GetAsync("https://localhost:6000/api/garantibank/bakiye/3");
+            string bakiye = await responseMessage.Content.ReadAsStringAsync();
+
+            ViewBag.Bakiye = bakiye;
             return View();
         }
     }
